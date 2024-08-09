955c4b91dc50dd461cd5595e11d5d74c859e0f74
bcorrie <bdcorrie@gmail.com>
2024-08-09 19:55:05 +0000

    Added ADC ID fields to be output in the AKC model.

--- src/scripts/akc-convert/repertoire.py
+++ src/scripts/akc-convert/repertoire.py
@@ -61,20 +61,126 @@ class Repertoire(Parser):
     #     is keyed on AKC class name, containing an array of instances of each class.
     # 
     # Example:
-    #     investigation_dict['PRJNA300878'] = {
-    #         'Investigation':{'PRJNA300878':[]},'Participant':[],'Specimen':[]}
+    #{
+    #  "PRJNA300878": {
+    #    "Investigation": {
+    #      "PRJNA300878": {
+    #        "akc_id": "",
+    #        "name": "Individual heritable ...",
+    #        "description": "The adaptive ..."
+    #        "study_type": {
+    #          "id": "NCIT:C16084",
+    #          "label": "Observational Study"
+    #        },
+    #        "archival_id": "PRJNA300878",
+    #        "inclusion_criteria": " ",
+    #        "release_date": "2019-09-03T17:01:10.226-05:00",
+    #        "update_date": "2022-12-16T20:42:08.879Z"
+    #      }
+    #    },
+    #    "Reference": {
+    #      "PRJNA300878": {
+    #        "source_uri": "",
+    #        "sources": "PMID:27005435"
+    #      }
+    #    },
+    #    "Participant": {
+    #      "TW01A": {
+    #        "akc_id": "",
+    #        "species": {
+    #          "id": "NCBITAXON:9606",
+    #          "label": "Homo sapiens"
+    #        },
+    #        "biological_sex": "female",
+    #        "age": 27,
+    #        "age_unit": {
+    #          "id": "UO:0000036",
+    #          "label": "year"
+    #        }
+    #      },
+    #      "TW01B": {
+    #        "akc_id": "",
+    #        "species": {
+    #          "id": "NCBITAXON:9606",
+    #          "label": "Homo sapiens"
+    #        },
+    #        "biological_sex": "female",
+    #        "age": 27,
+    #        "age_unit": {
+    #          "id": "UO:0000036",
+    #          "label": "year"
+    #        }
+    #      }
+    #    },
+    #    "Specimen": {
+    #      "TW01A_B_naive": {
+    #        "akc_id": "",
+    #        "tissue": {
+    #          "id": "UBERON:0013756",
+    #          "label": "venous blood"
+    #        }
+    #      },
+    #      "TW01A_B_memory": {
+    #        "akc_id": "",
+    #        "tissue": {
+    #          "id": "UBERON:0013756",
+    #          "label": "venous blood"
+    #        }
+    #      }
+    #    }
+    #  }
+    #}
     def generateAKCInvestigation(self, repertoire_dict, investigation_dict, akc_class_list):
         
         # Check to make sure we have an AIRR Study ID.
+        # TODO: This should be in the config file and not hardcoded here
         airr_study_field = 'study_id'
         if not airr_study_field in repertoire_dict:
             print('ERROR: Could not find AIRR study field %s in Repertoire'%(airr_study_field))
             return investigation_dict
+        # Get the repertoirem study, subject, and sample ID fields from the AIRR world
+        # TODO: This should be in the config file and not hardcoded here
+        if 'repertoire_id' in repertoire_dict:
+            repertoire_id = repertoire_dict['repertoire_id']['value']
+        else:
+            print('ERROR: Could not find repertoire_id field, skipping')
+            return investigation_dict
+
+        if 'sample_processing_id' in repertoire_dict:
+            sample_processing_id = repertoire_dict['sample_processing_id']['value']
+        else:
+            print('Warning: Could not find sample_processing_id field, skipping')
+            sample_processing_id = None
+
+        if 'data_processing_id' in repertoire_dict:
+            data_processing_id = repertoire_dict['data_processing_id']['value']
+        else:
+            print('Warning: Could not find data_processing_id field, skipping')
+            data_processing_id = None
+
+        if 'study_id' in repertoire_dict:
+            study_id = repertoire_dict['study_id']['value']
+        else:
+            print('ERROR: Could not find study_id field, skipping')
+            return investigation_dict
+
+        if 'sample_id' in repertoire_dict:
+            sample_id = repertoire_dict['sample_id']['value']
+        else:
+            print('ERROR: Could not find sample_id field, skipping')
+            return investigation_dict
+
+        if 'subject_id' in repertoire_dict:
+            subject_id = repertoire_dict['subject_id']['value']
+        else:
+            print('ERROR: Could not find subject_id field, skipping')
+            return investigation_dict
+
 
         # Check to see if the Investigation with that ID is in the dictionary.
-        if repertoire_dict['study_id']['value'] in investigation_dict:
-            # If it does, we need to kee track of it
-            investigation = investigation_dict[repertoire_dict['study_id']['value']]
+        if study_id in investigation_dict:
+            # If it does, we need to keep track of it
+            investigation = investigation_dict[study_id]
         else:
             # If it doesn't exist we need to create a dict for this study.
             investigation = dict()
@@ -84,13 +190,15 @@ class Repertoire(Parser):
                 investigation[akc_class] = dict()
 
             # Record the investigation for later use.
-            investigation_dict[repertoire_dict['study_id']['value']] = investigation
+            investigation_dict[study_id] = investigation
 
         # Iterate over the AKC classes and build the objects we need
         for akc_class in akc_class_list:
             # Get the link/key fields for the AKC classes.
-            # TODO: this should be in the config file.
+            # TODO: This should be in the config file and not hardcoded here
             if akc_class == 'Investigation':
+                airr_link_field = 'study_id'
+            elif akc_class == 'Reference':
                 airr_link_field = 'study_id'
             elif akc_class == 'Participant':
                 airr_link_field = 'subject_id'
@@ -104,9 +212,6 @@ class Repertoire(Parser):
                 print('ERROR: Could not find link field %s, skipping'%(airr_link_field))
                 continue
 
-            # Example:
-            #     investigation_dict['PRJNA300878'] = {
-            #         'Investigation':{'PRJNA300878':[]},'Participant':[],'Specimen':[]}
             airr_link_value = repertoire_dict[airr_link_field]['value']
             akc_dict = investigation[akc_class]
             # Check to see if we have seen this instance before
@@ -123,31 +228,34 @@ class Repertoire(Parser):
                 # process it.
                 if 'akc_class' in value and value['akc_class'] == akc_class:
                     akc_object[value['akc_field']] = value['value']
-                    akc_dict[airr_link_value] = akc_object
                     print('Info: processing class %s, airr = %s, akc = %s, value = %s'%(akc_class, key, value['akc_field'],value['value']))
+                    if akc_class == 'Investigation':
+                        akc_object['adc_repertoire_id'] = repertoire_id
+                        akc_object['adc_sample_processing_id'] = sample_processing_id
+                        akc_object['adc_data_processing_id'] = data_processing_id
+                    if akc_class == 'Reference':
+                        akc_object['adc_repertoire_id'] = repertoire_id
+                        akc_object['adc_sample_processing_id'] = sample_processing_id
+                        akc_object['adc_data_processing_id'] = data_processing_id
+                        akc_object['adc_study_id'] = study_id
+                    if akc_class == 'Participant':
+                        akc_object['adc_repertoire_id'] = repertoire_id
+                        akc_object['adc_sample_processing_id'] = sample_processing_id
+                        akc_object['adc_data_processing_id'] = data_processing_id
+                        akc_object['adc_study_id'] = study_id
+                        akc_object['adc_subject_id'] = subject_id
+                    if akc_class == 'Specimen':
+                        akc_object['adc_repertoire_id'] = repertoire_id
+                        akc_object['adc_sample_processing_id'] = sample_processing_id
+                        akc_object['adc_data_processing_id'] = data_processing_id
+                        akc_object['adc_study_id'] = study_id
+                        akc_object['adc_subject_id'] = subject_id
+                        akc_object['adc_sample_id'] = sample_id
+                    akc_dict[airr_link_value] = akc_object
 
+            # Once processing done for this investigation, reassign the investigation dictionary
+            investigation[akc_class] = akc_dict
 
-#        # Create a dictionary that contains the AKC class objects, keyed on
-#        # AKC class name. We iterate over the list of class names provided.
-#        akc_class_dict = dict()
-#        for akc_class in akc_class_list:
-#        #    # Our classes are in the global scope, so we can instantiate
-#        #    # a class using the globals function with the class name as key.
-#            akc_class_dict[akc_class] = globals()[akc_class]('')
-#
-#        # For each class, we traverse each field to see if it belongs in
-#        # the class, if so we set the field in the class to be the
-#        # appropriate value.
-#        for class_key, class_value in akc_class_dict.items():
-#            print('Info: Processing class %s'%(class_key))
-#            # Iterate over the fields in the repertoire
-#            for key, value in repertoire_dict.items():
-#                # If the field has an akc_class and it is the current
-#                # class that we are processing, then set the field (value['akc_field'])
-#                # in the class (akc_class_dict[class_key]) to be the correct
-#                # value (value['value'])
-#                if 'akc_class' in value and value['akc_class'] == class_key:
-#                    akc_class_dict[class_key][value['akc_field']] = value['value']
-
+        # Return the investigation dictionary we have built.
         return investigation_dict
 
