faa7efffed9b3d86824172cc24cad66dc0337bc7
bcorrie <bdcorrie@gmail.com>
2024-11-13 23:51:13 +0000

    Remove deprecated and no longer used method.

--- src/scripts/akc-convert/repertoire.py
+++ src/scripts/akc-convert/repertoire.py
@@ -33,40 +33,6 @@ class Repertoire(Parser):
         # TODO: This should be in a config file and not harcoded here.
         self.link_classes = ['LifeEvent', 'StudyEvent']
 
-    # Hide the impementation of AKC data model from the other classes. 
-    # repertoire_dict: Keyed on the AIRR field name, contains three attributes:
-    #     value: the value of the AIRR field
-    #     akc_field: the field name for the field in the AKC data model
-    #     akc_class: the AKC class/object the field belongs to in the AKC data model
-    # NOTE: Deprecetated, not in use.
-    def generateAKCRepertoire(self, repertoire_dict, akc_class_list):
-        
-        # Create a dictionary that contains the AKC class objects, keyed on
-        # AKC class name. We iterate over the list of class names provided.
-        akc_class_dict = dict()
-        for akc_class in akc_class_list:
-            # Our classes are in the global scope, so we can instantiate
-            # a class using the globals function with the class name as key.
-            akc_class_dict[akc_class] = globals()[akc_class]('')
-
-        # For each class, we traverse each field to see if it belongs in
-        # the class, if so we set the field in the class to be the
-        # appropriate value.
-        for class_key, class_value in akc_class_dict.items():
-            print('Info: Processing class %s'%(class_key))
-            # Iterate over the fields in the repertoire
-            for key, value in repertoire_dict.items():
-                # If the field has an akc_class and it is the current
-                # class that we are processing, then set the field (value['akc_field'])
-                # in the class (akc_class_dict[class_key]) to be the correct
-                # value (value['value'])
-                if 'akc_class' in value and value['akc_class'] == class_key:
-                    akc_class_dict[class_key][value['akc_field']] = value['value']
-        #print(yaml_dumper.dumps(akc_class_dict))
-        print(json_dumper.dumps(akc_class_dict))
-
-        return True
-
     def getAIRRUniqueLink(self, repertoire_dict, link_class, akc_class):
         # Get the link name for a class instance. Default to None
         airr_link_value = None
