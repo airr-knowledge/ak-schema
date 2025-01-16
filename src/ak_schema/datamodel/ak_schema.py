855c88188067f317df6e49207e46fbc6e1c16e90
Scott Christley <scott.christley@utsouthwestern.edu>
2025-01-15 22:10:22 -0600

    generated files

--- src/ak_schema/datamodel/ak_schema.py
+++ src/ak_schema/datamodel/ak_schema.py
@@ -1,5 +1,5 @@
 # Auto generated from ak_schema.yaml by pythongen.py version: 0.0.1
-# Generation date: 2025-01-15T19:52:44
+# Generation date: 2025-01-16T04:02:24
 # Schema: ak-schema
 #
 # id: https://github.com/airr-knowledge/ak-schema
@@ -31,7 +31,7 @@ version = None
 dataclasses._init_fn = dataclasses_init_fn_with_kwargs
 
 # Namespaces
-AKC = CurieNamespace('AKC', 'https://airr-knowledge.org/akc/api/v1/id/')
+AKC = CurieNamespace('AKC', 'https://airr-knowledge.org/airrkb/api/v1/id/')
 APOLLO_SV = CurieNamespace('APOLLO_SV', 'http://purl.obolibrary.org/obo/APOLLO_SV_')
 BFO = CurieNamespace('BFO', 'http://purl.obolibrary.org/obo/BFO_')
 CL = CurieNamespace('CL', 'http://purl.obolibrary.org/obo/CL_')
@@ -47,6 +47,7 @@ OGMS = CurieNamespace('OGMS', 'http://purl.obolibrary.org/obo/OGMS_')
 OMRSE = CurieNamespace('OMRSE', 'http://purl.obolibrary.org/obo/OMRSE_')
 ONTIE = CurieNamespace('ONTIE', 'https://ontology.iedb.org/ontology/ONTIE_')
 PATO = CurieNamespace('PATO', 'http://purl.obolibrary.org/obo/PATO_')
+PMID = CurieNamespace('PMID', 'https://pubmed.ncbi.nlm.nih.gov/')
 RO = CurieNamespace('RO', 'http://purl.obolibrary.org/obo/RO_')
 SBO = CurieNamespace('SBO', 'http://purl.obolibrary.org/obo/SBO_')
 UBERON = CurieNamespace('UBERON', 'http://purl.obolibrary.org/obo/UBERON_')
@@ -478,6 +479,74 @@ class PlanSpecification(NamedThing):
     akc_id: Union[str, PlanSpecificationAkcId] = None
 
 @dataclass(repr=False)
+class AIRRKnowledgeCommons(YAMLRoot):
+    """
+    A container for instances of multiple classes.
+    """
+    _inherited_slots: ClassVar[List[str]] = []
+
+    class_class_uri: ClassVar[URIRef] = AK_SCHEMA["AIRRKnowledgeCommons"]
+    class_class_curie: ClassVar[str] = "ak_schema:AIRRKnowledgeCommons"
+    class_name: ClassVar[str] = "AIRRKnowledgeCommons"
+    class_model_uri: ClassVar[URIRef] = AK_SCHEMA.AIRRKnowledgeCommons
+
+    investigations: Optional[Union[Dict[Union[str, InvestigationAkcId], Union[dict, "Investigation"]], List[Union[dict, "Investigation"]]]] = empty_dict()
+    references: Optional[Union[Dict[Union[str, ReferenceSourceUri], Union[dict, "Reference"]], List[Union[dict, "Reference"]]]] = empty_dict()
+    study_arms: Optional[Union[Dict[Union[str, StudyArmAkcId], Union[dict, "StudyArm"]], List[Union[dict, "StudyArm"]]]] = empty_dict()
+    study_events: Optional[Union[Dict[Union[str, StudyEventAkcId], Union[dict, "StudyEvent"]], List[Union[dict, "StudyEvent"]]]] = empty_dict()
+    participants: Optional[Union[Dict[Union[str, ParticipantAkcId], Union[dict, "Participant"]], List[Union[dict, "Participant"]]]] = empty_dict()
+    life_events: Optional[Union[Dict[Union[str, LifeEventAkcId], Union[dict, "LifeEvent"]], List[Union[dict, "LifeEvent"]]]] = empty_dict()
+    immune_exposures: Optional[Union[Dict[Union[str, ImmuneExposureAkcId], Union[dict, "ImmuneExposure"]], List[Union[dict, "ImmuneExposure"]]]] = empty_dict()
+    assessments: Optional[Union[Dict[Union[str, AssessmentAkcId], Union[dict, "Assessment"]], List[Union[dict, "Assessment"]]]] = empty_dict()
+    specimens: Optional[Union[Dict[Union[str, SpecimenAkcId], Union[dict, "Specimen"]], List[Union[dict, "Specimen"]]]] = empty_dict()
+    specimen_collections: Optional[Union[Dict[Union[str, SpecimenCollectionAkcId], Union[dict, "SpecimenCollection"]], List[Union[dict, "SpecimenCollection"]]]] = empty_dict()
+    specimen_processings: Optional[Union[Dict[Union[str, SpecimenProcessingAkcId], Union[dict, "SpecimenProcessing"]], List[Union[dict, "SpecimenProcessing"]]]] = empty_dict()
+    assays: Optional[Union[Dict[Union[str, AssayAkcId], Union[dict, "Assay"]], List[Union[dict, "Assay"]]]] = empty_dict()
+    datasets: Optional[Union[Dict[Union[str, DatasetAkcId], Union[dict, "Dataset"]], List[Union[dict, "Dataset"]]]] = empty_dict()
+    conclusions: Optional[Union[Dict[Union[str, ConclusionAkcId], Union[dict, "Conclusion"]], List[Union[dict, "Conclusion"]]]] = empty_dict()
+    chains: Optional[Union[Dict[Union[str, ChainAkcId], Union[dict, "Chain"]], List[Union[dict, "Chain"]]]] = empty_dict()
+    tcell_receptors: Optional[Union[Dict[Union[str, TCellReceptorAkcId], Union[dict, "TCellReceptor"]], List[Union[dict, "TCellReceptor"]]]] = empty_dict()
+    epitopes: Optional[Union[Dict[Union[str, EpitopeAkcId], Union[dict, "Epitope"]], List[Union[dict, "Epitope"]]]] = empty_dict()
+
+    def __post_init__(self, *_: List[str], **kwargs: Dict[str, Any]):
+        self._normalize_inlined_as_dict(slot_name="investigations", slot_type=Investigation, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="references", slot_type=Reference, key_name="source_uri", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="study_arms", slot_type=StudyArm, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="study_events", slot_type=StudyEvent, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="participants", slot_type=Participant, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="life_events", slot_type=LifeEvent, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="immune_exposures", slot_type=ImmuneExposure, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="assessments", slot_type=Assessment, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="specimens", slot_type=Specimen, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="specimen_collections", slot_type=SpecimenCollection, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="specimen_processings", slot_type=SpecimenProcessing, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="assays", slot_type=Assay, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="datasets", slot_type=Dataset, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="conclusions", slot_type=Conclusion, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="chains", slot_type=Chain, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="tcell_receptors", slot_type=TCellReceptor, key_name="akc_id", keyed=True)
+
+        self._normalize_inlined_as_dict(slot_name="epitopes", slot_type=Epitope, key_name="akc_id", keyed=True)
+
+        super().__post_init__(**kwargs)
+
+
+@dataclass(repr=False)
 class Investigation(PlannedProcess):
     """
     A scientific investigation.
@@ -677,9 +746,6 @@ class Participant(NamedThing):
         if self.study_arm is not None and not isinstance(self.study_arm, StudyArmAkcId):
             self.study_arm = StudyArmAkcId(self.study_arm)
 
-        if self.biological_sex is not None and not isinstance(self.biological_sex, BiologicalSexOntology):
-            self.biological_sex = BiologicalSexOntology(self.biological_sex)
-
         if self.phenotypic_sex is not None and not isinstance(self.phenotypic_sex, PhenotypicSexOntology):
             self.phenotypic_sex = PhenotypicSexOntology(self.phenotypic_sex)
 
@@ -767,9 +833,6 @@ class LifeEvent(NamedThing):
         if self.study_event is not None and not isinstance(self.study_event, StudyEventAkcId):
             self.study_event = StudyEventAkcId(self.study_event)
 
-        if self.life_event_type is not None and not isinstance(self.life_event_type, LifeEventProcessOntology):
-            self.life_event_type = LifeEventProcessOntology(self.life_event_type)
-
         if self.geolocation is not None and not isinstance(self.geolocation, GeolocationOntology):
             self.geolocation = GeolocationOntology(self.geolocation)
 
@@ -818,12 +881,6 @@ class ImmuneExposure(NamedThing):
 
         if self.life_event is not None and not isinstance(self.life_event, LifeEventAkcId):
             self.life_event = LifeEventAkcId(self.life_event)
-
-        if self.exposure_material is not None and not isinstance(self.exposure_material, ExposureMaterialOntology):
-            self.exposure_material = ExposureMaterialOntology(self.exposure_material)
-
-        if self.disease is not None and not isinstance(self.disease, DiseaseOntology):
-            self.disease = DiseaseOntology(self.disease)
 
         if self.disease_stage is not None and not isinstance(self.disease_stage, str):
             self.disease_stage = str(self.disease_stage)
@@ -4377,13 +4434,6 @@ class SampleProcessing(YAMLRoot):
 # Enumerations
 class BiologicalSexOntology(EnumDefinitionImpl):
 
-    female = PermissibleValue(
-        text="female",
-        meaning=PATO["0020002"])
-    male = PermissibleValue(
-        text="male",
-        meaning=PATO["0020001"])
-
     _defn = EnumDefinition(
         name="BiologicalSexOntology",
     )
@@ -4665,86 +4715,17 @@ class LifeEventProcessOntology(EnumDefinitionImpl):
         name="LifeEventProcessOntology",
     )
 
-    @classmethod
-    def _addvals(cls):
-        setattr(cls, "Documented exposure without evidence for disease",
-            PermissibleValue(
-                text="Documented exposure without evidence for disease",
-                meaning=ONTIE["0003305"]))
-        setattr(cls, "Environmental exposure to endemic/ubiquitous agent without evidence for disease",
-            PermissibleValue(
-                text="Environmental exposure to endemic/ubiquitous agent without evidence for disease",
-                meaning=ONTIE["0003308"]))
-        setattr(cls, "Exposure with existing immune reactivity without evidence for disease",
-            PermissibleValue(
-                text="Exposure with existing immune reactivity without evidence for disease",
-                meaning=OBI["1110061"]))
-        setattr(cls, "specimen collection",
-            PermissibleValue(
-                text="specimen collection",
-                meaning=OBI["0000659"]))
-
 class ExposureMaterialOntology(EnumDefinitionImpl):
-
-    Dryvax = PermissibleValue(
-        text="Dryvax",
-        meaning=VO["0000035"])
 
     _defn = EnumDefinition(
         name="ExposureMaterialOntology",
     )
 
-    @classmethod
-    def _addvals(cls):
-        setattr(cls, "Influenza A virus",
-            PermissibleValue(
-                text="Influenza A virus",
-                meaning=NCBITAXON["11320"]))
-        setattr(cls, "Mycobacterium tuberculosis",
-            PermissibleValue(
-                text="Mycobacterium tuberculosis",
-                meaning=NCBITAXON["1773"]))
-        setattr(cls, "Plasmodium falciparum",
-            PermissibleValue(
-                text="Plasmodium falciparum",
-                meaning=NCBITAXON["5833"]))
-        setattr(cls, "Human gammaherpesvirus 4",
-            PermissibleValue(
-                text="Human gammaherpesvirus 4",
-                meaning=NCBITAXON["10376"]))
-        setattr(cls, "Human betaherpesvirus 5",
-            PermissibleValue(
-                text="Human betaherpesvirus 5",
-                meaning=NCBITAXON["10359"]))
-        setattr(cls, "Diphtheria-Tetanus-Pertussis vaccine",
-            PermissibleValue(
-                text="Diphtheria-Tetanus-Pertussis vaccine",
-                meaning=VO["0000738"]))
-
 class DiseaseOntology(EnumDefinitionImpl):
-
-    healthy = PermissibleValue(
-        text="healthy",
-        meaning=ONTIE["000342"])
 
     _defn = EnumDefinition(
         name="DiseaseOntology",
     )
-
-    @classmethod
-    def _addvals(cls):
-        setattr(cls, "COVID-19",
-            PermissibleValue(
-                text="COVID-19",
-                meaning=DOID["0080600"]))
-        setattr(cls, "Plasmodium falciparum malaria",
-            PermissibleValue(
-                text="Plasmodium falciparum malaria",
-                meaning=DOID["14067"]))
-        setattr(cls, "dengue hemorrhagic fever",
-            PermissibleValue(
-                text="dengue hemorrhagic fever",
-                meaning=DOID["12206"]))
 
 class DiseaseStageOntology(EnumDefinitionImpl):
 
@@ -6728,3 +6709,54 @@ slots.reactivity_value = Slot(uri=AK_SCHEMA.reactivity_value, name="reactivity_v
 
 slots.reactivity_unit = Slot(uri=AK_SCHEMA.reactivity_unit, name="reactivity_unit", curie=AK_SCHEMA.curie('reactivity_unit'),
                    model_uri=AK_SCHEMA.reactivity_unit, domain=None, range=Optional[str])
+
+slots.aIRRKnowledgeCommons__investigations = Slot(uri=AK_SCHEMA.investigations, name="aIRRKnowledgeCommons__investigations", curie=AK_SCHEMA.curie('investigations'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__investigations, domain=None, range=Optional[Union[Dict[Union[str, InvestigationAkcId], Union[dict, Investigation]], List[Union[dict, Investigation]]]])
+
+slots.aIRRKnowledgeCommons__references = Slot(uri=AK_SCHEMA.references, name="aIRRKnowledgeCommons__references", curie=AK_SCHEMA.curie('references'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__references, domain=None, range=Optional[Union[Dict[Union[str, ReferenceSourceUri], Union[dict, Reference]], List[Union[dict, Reference]]]])
+
+slots.aIRRKnowledgeCommons__study_arms = Slot(uri=AK_SCHEMA.study_arms, name="aIRRKnowledgeCommons__study_arms", curie=AK_SCHEMA.curie('study_arms'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__study_arms, domain=None, range=Optional[Union[Dict[Union[str, StudyArmAkcId], Union[dict, StudyArm]], List[Union[dict, StudyArm]]]])
+
+slots.aIRRKnowledgeCommons__study_events = Slot(uri=AK_SCHEMA.study_events, name="aIRRKnowledgeCommons__study_events", curie=AK_SCHEMA.curie('study_events'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__study_events, domain=None, range=Optional[Union[Dict[Union[str, StudyEventAkcId], Union[dict, StudyEvent]], List[Union[dict, StudyEvent]]]])
+
+slots.aIRRKnowledgeCommons__participants = Slot(uri=AK_SCHEMA.participants, name="aIRRKnowledgeCommons__participants", curie=AK_SCHEMA.curie('participants'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__participants, domain=None, range=Optional[Union[Dict[Union[str, ParticipantAkcId], Union[dict, Participant]], List[Union[dict, Participant]]]])
+
+slots.aIRRKnowledgeCommons__life_events = Slot(uri=AK_SCHEMA.life_events, name="aIRRKnowledgeCommons__life_events", curie=AK_SCHEMA.curie('life_events'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__life_events, domain=None, range=Optional[Union[Dict[Union[str, LifeEventAkcId], Union[dict, LifeEvent]], List[Union[dict, LifeEvent]]]])
+
+slots.aIRRKnowledgeCommons__immune_exposures = Slot(uri=AK_SCHEMA.immune_exposures, name="aIRRKnowledgeCommons__immune_exposures", curie=AK_SCHEMA.curie('immune_exposures'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__immune_exposures, domain=None, range=Optional[Union[Dict[Union[str, ImmuneExposureAkcId], Union[dict, ImmuneExposure]], List[Union[dict, ImmuneExposure]]]])
+
+slots.aIRRKnowledgeCommons__assessments = Slot(uri=AK_SCHEMA.assessments, name="aIRRKnowledgeCommons__assessments", curie=AK_SCHEMA.curie('assessments'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__assessments, domain=None, range=Optional[Union[Dict[Union[str, AssessmentAkcId], Union[dict, Assessment]], List[Union[dict, Assessment]]]])
+
+slots.aIRRKnowledgeCommons__specimens = Slot(uri=AK_SCHEMA.specimens, name="aIRRKnowledgeCommons__specimens", curie=AK_SCHEMA.curie('specimens'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__specimens, domain=None, range=Optional[Union[Dict[Union[str, SpecimenAkcId], Union[dict, Specimen]], List[Union[dict, Specimen]]]])
+
+slots.aIRRKnowledgeCommons__specimen_collections = Slot(uri=AK_SCHEMA.specimen_collections, name="aIRRKnowledgeCommons__specimen_collections", curie=AK_SCHEMA.curie('specimen_collections'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__specimen_collections, domain=None, range=Optional[Union[Dict[Union[str, SpecimenCollectionAkcId], Union[dict, SpecimenCollection]], List[Union[dict, SpecimenCollection]]]])
+
+slots.aIRRKnowledgeCommons__specimen_processings = Slot(uri=AK_SCHEMA.specimen_processings, name="aIRRKnowledgeCommons__specimen_processings", curie=AK_SCHEMA.curie('specimen_processings'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__specimen_processings, domain=None, range=Optional[Union[Dict[Union[str, SpecimenProcessingAkcId], Union[dict, SpecimenProcessing]], List[Union[dict, SpecimenProcessing]]]])
+
+slots.aIRRKnowledgeCommons__assays = Slot(uri=AK_SCHEMA.assays, name="aIRRKnowledgeCommons__assays", curie=AK_SCHEMA.curie('assays'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__assays, domain=None, range=Optional[Union[Dict[Union[str, AssayAkcId], Union[dict, Assay]], List[Union[dict, Assay]]]])
+
+slots.aIRRKnowledgeCommons__datasets = Slot(uri=AK_SCHEMA.datasets, name="aIRRKnowledgeCommons__datasets", curie=AK_SCHEMA.curie('datasets'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__datasets, domain=None, range=Optional[Union[Dict[Union[str, DatasetAkcId], Union[dict, Dataset]], List[Union[dict, Dataset]]]])
+
+slots.aIRRKnowledgeCommons__conclusions = Slot(uri=AK_SCHEMA.conclusions, name="aIRRKnowledgeCommons__conclusions", curie=AK_SCHEMA.curie('conclusions'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__conclusions, domain=None, range=Optional[Union[Dict[Union[str, ConclusionAkcId], Union[dict, Conclusion]], List[Union[dict, Conclusion]]]])
+
+slots.aIRRKnowledgeCommons__chains = Slot(uri=AK_SCHEMA.chains, name="aIRRKnowledgeCommons__chains", curie=AK_SCHEMA.curie('chains'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__chains, domain=None, range=Optional[Union[Dict[Union[str, ChainAkcId], Union[dict, Chain]], List[Union[dict, Chain]]]])
+
+slots.aIRRKnowledgeCommons__tcell_receptors = Slot(uri=AK_SCHEMA.tcell_receptors, name="aIRRKnowledgeCommons__tcell_receptors", curie=AK_SCHEMA.curie('tcell_receptors'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__tcell_receptors, domain=None, range=Optional[Union[Dict[Union[str, TCellReceptorAkcId], Union[dict, TCellReceptor]], List[Union[dict, TCellReceptor]]]])
+
+slots.aIRRKnowledgeCommons__epitopes = Slot(uri=AK_SCHEMA.epitopes, name="aIRRKnowledgeCommons__epitopes", curie=AK_SCHEMA.curie('epitopes'),
+                   model_uri=AK_SCHEMA.aIRRKnowledgeCommons__epitopes, domain=None, range=Optional[Union[Dict[Union[str, EpitopeAkcId], Union[dict, Epitope]], List[Union[dict, Epitope]]]])
